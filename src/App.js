4a6ff36feb837dc89c4ff5a488a0962c00b2f004
jmingorance <joan_mingorance_cortes@hotmail.com>
2023-11-28 19:21:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  const titulo = <h1>Computer vision</h1>
+  const label = <label>Insert URL or type prompt</label>
+  const textarea = <textarea></textarea>
+  const btnAnalyze = <button>Analyze</button>;
+  const btnGenerate = <button>Generate</button>;
+
+  return titulo, label, textarea, btnAnalyze, btnGenerate;
 }
 
 export default App;
